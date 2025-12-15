a2272cb6763760c41664b5dd5374c119eb7a439c
Capy <dimak12k@yandex.ru>
2025-12-15 20:16:19 +0300

    Add Python functional test for /auth/v0/sign-up

--- tests/functional/test_basic.py
+++ tests/functional/test_basic.py
@@ -1,7 +1,22 @@
 # Start via `make test-debug` or `make test-release`
 
 
-async def test_basic(service_client):
-    response = await service_client.post('/hello', params={'name': 'Tester'})
+async def test_sign_up_basic(service_client):
+    payload = {
+        "username": "dima_test",
+        "email": "dima@example.com",
+        "password": "StrongPass123!",
+    }
+
+    response = await service_client.post(
+        '/auth/v0/sign-up',
+        json=payload,
+    )
+
     assert response.status == 200
-    assert response.text == 'Hello, Tester!\n'
+
+    body = response.json()
+    assert body["user"]["username"] == "dima_test"
+    assert body["user"]["email"] == "dima@example.com"
+    assert body["user"]["is_email_verified"] is False
+    assert body["user"]["is_phone_verified"] is False
