5c45f654c101253bade1a20014252c5569836951
Capy <dimak12k@yandex.ru>
2025-12-23 19:26:29 +0300

    Implement PostgresUserRepository users operations

--- src/auth/infra/db/pg/postgres_user_repository.cpp
+++ src/auth/infra/db/pg/postgres_user_repository.cpp
@@ -1,21 +1,38 @@
 #include <auth/infra/db/pg/postgres_user_repository.hpp>
 #include <userver/storages/postgres/cluster.hpp>
 
+#include <smirkly::auth/sql_queries.hpp>
+
 namespace smirkly::auth::infra::db::pg {
     PostgresUserRepository::PostgresUserRepository(USERVER_NAMESPACE::storages::postgres::ClusterPtr pg_cluster)
         : pg_cluster_(std::move(pg_cluster)) {
     }
 
     [[nodiscard]] bool PostgresUserRepository::ExistsByUsername(std::string_view username) {
-        return false;
+        auto res = pg_cluster_->Execute(
+            USERVER_NAMESPACE::storages::postgres::ClusterHostType::kSlave,
+            sql::kUsersExistsByUsername,
+            username
+        );
+        return res.AsSingleRow<bool>();
     }
 
     [[nodiscard]] bool PostgresUserRepository::ExistsByEmail(std::string_view email) {
-        return false;
+        auto res = pg_cluster_->Execute(
+            USERVER_NAMESPACE::storages::postgres::ClusterHostType::kSlave,
+            sql::kUsersExistsByEmail,
+            email
+        );
+        return res.AsSingleRow<bool>();
     }
 
     [[nodiscard]] bool PostgresUserRepository::ExistsByPhone(std::string_view phone) {
-        return false;
+        auto res = pg_cluster_->Execute(
+            USERVER_NAMESPACE::storages::postgres::ClusterHostType::kSlave,
+            sql::kUsersExistsByPhone,
+            phone
+        );
+        return res.AsSingleRow<bool>();
     }
 
     [[nodiscard]] std::optional<domain::models::User> PostgresUserRepository::FindById(std::string_view id) {
